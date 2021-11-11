d0c909e7f94a5cc02c1f8da7edb6758bd770350e
kwatch <kwatch@gmail.com>
2021-11-11 09:39:11 +0900

    ruby: change 'Benchmarker.new()' to consider backward compatibility

--- ruby/lib/benchmarker.rb
+++ ruby/lib/benchmarker.rb
@@ -17,16 +17,14 @@ module Benchmarker
   OPTIONS = {}    # ex: {loop: 1000, iter: 10, extra: 2, inverse: true}
 
   def self.new(title=nil, **kwargs, &b)
+    if block_given?()    # for backward compatibility
+      return self.scope(title, **kwargs, &b)
+    end
     #; [!s7y6x] overwrites existing options by command-line options.
     d = OPTIONS
     kwargs.update(OPTIONS)
     #; [!2zh7w] creates new Benchmark object wit options.
     bm = Benchmark.new(title: title, **kwargs)
-    ## backward compatibility
-    #if block_given?()
-    #  bm.scope(&b)
-    #  bm.run()
-    #end
     return bm
   end
 
